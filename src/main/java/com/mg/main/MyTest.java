b8f2c4fd9e7d9b67fa07b8e2c80e47bc2ab86df5
seekzed@gmail.com <seekzed@gmail.com@d3a69622-7479-11e7-8d9c-6332a8a82ef2>
2017-08-18 21:57:57 +0000

    HTML inside Webapp

---
+++ src/main/java/com/mg/main/MyTest.java
@@ -0,0 +1,39 @@
+package com.mg.main;
+
+public class MyTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String SHADOW_AUTH_TEMPLATE={"TEMP_01=Value1","TEMP_02=Value2","TEMP_03=Value3"};
+		
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+
+	}
+
+}
